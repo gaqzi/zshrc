6d995cdad5f75dc6c96c0e667056e95d3c04881c
Björn Andersson <ba@sanitarium.se>
2013-02-26 11:29:45 +0800

    up to date

--- plugins/virtualenvwrapper.plugin.zsh
+++ plugins/virtualenvwrapper.plugin.zsh
@@ -1,1 +1,12 @@
-[[ -s "/usr/local/bin/virtualenvwrapper.sh" ]] && source "/usr/local/bin/virtualenvwrapper.sh"
+if [[ -s "/usr/local/bin/virtualenvwrapper.sh" ]] ; then
+    source "/usr/local/bin/virtualenvwrapper.sh"
+    has_virtualenv() {
+        if [ -e .venv ]; then
+            workon `cat .venv`
+        fi
+    }
+    venv_cd () {
+        builtin cd "$@" && has_virtualenv
+    }
+    alias cd='venv_cd'
+fi
