01d35ae2c561702299537f20ec29eef97364d685
Björn Andersson <ba@sanitarium.se>
2014-06-22 08:23:16 +0800

    Add dynamic library path for MySQL OSX export

--- plugins/mysql-mac.plugin.zsh
+++ plugins/mysql-mac.plugin.zsh
@@ -1,4 +1,5 @@
 # When using the official MySQL installation package for OSX
-if [[ $OSTYPE =~ darwin ]] ; then
-	PATH=$PATH:/usr/local/mysql/bin
+if [[ $OSTYPE =~ darwin ]] && [[ -d /usr/local/mysql ]]; then
+    export PATH=$PATH:/usr/local/mysql/bin
+    export DYLD_LIBRARY_PATH=/usr/local/mysql/lib:$DYLD_LIBRARY_PATH
 fi
