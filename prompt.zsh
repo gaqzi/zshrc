c6d4ca4a241d6283e2cf304ad58eb3dfe57061fb
Björn Andersson <ba@sanitarium.se>
2014-08-08 23:41:21 +0800

    Show rbenv ruby version in prompt if available

--- prompt.zsh
+++ prompt.zsh
@@ -6,7 +6,19 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="]"
 ZSH_THEME_GIT_PROMPT_DIRTY="*"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-PROMPT='%B%n%b@%m$(git_prompt_info)$(rvm_prompt_info)%# '
+# Handles both rvm and rbenv
+function ruby_prompt_info() {
+    _prompt=`rvm_prompt_info`
+    if [ -z $_prompt ] ; then
+        _prompt=`rbenv_prompt_info`
+    fi
+
+    if [ -n $_prompt ] ; then
+        echo "($_prompt)"
+    fi
+}
+
+PROMPT='%B%n%b@%m$(git_prompt_info)$(ruby_prompt_info)%# '
 RPROMPT='(%2~)'
 case $TERM in
     cons*|dumb)
