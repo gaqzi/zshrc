0f05f55c0f6ab721a9e2b4427420c4b25090bdb0
Björn Andersson <ba@sanitarium.se>
2014-08-31 12:49:45 +0800

    Remove old history searching from conf

--- lib/history-navigation.zsh
+++ lib/history-navigation.zsh
@@ -8,10 +8,6 @@ setopt HIST_IGNORE_SPACE
 setopt HIST_SAVE_NO_DUPS
 setopt SHARE_HISTORY
 
-autoload history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-
 bindkey "\e[A" history-substring-search-up   #cursor up
 bindkey "\e[B" history-substring-search-down #cursor down
 bindkey ' ' magic-space    # also do history expansion on space
