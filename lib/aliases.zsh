2c4ce2616bda3cadab32755943da5c4189ad39e6
Björn Andersson <ba@sanitarium.se>
2017-03-01 09:43:09 +0800

    Add some git aliases
    I have used these for a while with oh-my-zsh at work

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -2,3 +2,8 @@ alias la='ls -a'
 alias ssc='ssh cell.sanitarium.se'
 alias serve-this-folder="python -m SimpleHTTPServer"
 alias bower='noglob bower'
+
+alias g='git'
+alias ga='git add'
+alias gap='git add --patch'
+alias gc='git commit'
